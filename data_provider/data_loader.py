1d2966e37f858ff72b857ea0aad39cd9ab4aa6c0
陈建国 <110661579+threebodyand@users.noreply.github.com>
2024-05-27 14:42:48 +0800

    Update data_loader.py

--- data_provider/data_loader.py
+++ data_provider/data_loader.py
@@ -190,21 +190,10 @@ class Dataset_ETT_minute(Dataset):
 
 
 class Dataset_Custom(Dataset):
-    '''自定义的 Dataset_Custom类， 继承父类：Dataset，重载两个私有成员函数__len__、__getitem__
-    参数:
-        root_path: './dataset/electricity/'
-        flag: 'train'
-        size: [96, 48, 96]
-        features: 'M'
-        data_path: 'electricity.csv'
-        timeenc: 1
-    '''
     def __init__(self, root_path, flag='train', size=None,
                  features='S', data_path='ETTh1.csv',
                  target='OT', scale=True, timeenc=0, freq='h'):
-        # size [seq_len, label_len, pred_len]
-        # info
-        if size == None:
+        if size is None:
             self.seq_len = 24 * 4 * 4
             self.label_len = 24 * 4
             self.pred_len = 24 * 4
@@ -212,7 +201,7 @@ class Dataset_Custom(Dataset):
             self.seq_len = size[0]
             self.label_len = size[1]
             self.pred_len = size[2]
-        # init
+
         assert flag in ['train', 'test', 'val']
         type_map = {'train': 0, 'val': 1, 'test': 2}
         self.set_type = type_map[flag]
@@ -228,57 +217,53 @@ class Dataset_Custom(Dataset):
         self.__read_data__()
 
     def __read_data__(self):
-        # 使经过处理的数据符合标准正态分布，即均值为0，标准差为1；(减均值，然后除以标准差)
         self.scaler = StandardScaler()
-        self.scaler = StandardScaler()
-        df_raw = pd.read_csv(os.path.join(self.root_path,
-                                          self.data_path))
+        df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
 
-        '''
-        df_raw.columns: ['date', ...(other features), target feature]
-        '''
-        cols = list(df_raw.columns)   # 列:['data','0',...,'321','OT']
-        cols.remove(self.target)      # 去除需要预测的特征（仅对预测单个值有效，对于电力数据集一次预测多个特征无效）
-        cols.remove('date')           # 去除 'date'（这是把原始数据里面的时间戳提出出来了）
-        df_raw = df_raw[['date'] + cols + [self.target]]    # 这不就是原来的 df_raw 吗？多此一举？
-        # print(cols)
-        # 按比例分配数据，训练集、验证集、测试集分别为7：2：1
-        num_train = int(len(df_raw) * 0.7)   # 假设一共100行， 70
-        num_test = int(len(df_raw) * 0.2)    # 20
+        cols = list(df_raw.columns)
+
+        if self.target in cols:
+            cols.remove(self.target)
+
+        if 'date' in cols:
+            cols.remove('date')
+
+        df_raw = df_raw[['date'] + cols + [self.target]]
+
+        num_train = int(len(df_raw) * 0.7)
+        num_test = int(len(df_raw) * 0.2)
         num_vali = len(df_raw) - num_train - num_test
-        # [0, 18316=18412-96, 20948=26304-5260-96]
-        border1s = [0, num_train - self.seq_len, len(df_raw) - num_test - self.seq_len]  # 假设 seq_len=5,  [0, 65, 75]
-        # [18412, 21044=18412+2632, 26304]
-        border2s = [num_train, num_train + num_vali, len(df_raw)]  # [70, 80, 100]
-        # border1=0 , border2=18412
-        border1 = border1s[self.set_type]   # train:0  # val:65 # test:75
-        border2 = border2s[self.set_type]   # train:70 # val:80 # test:100 这里验证集会有泄漏，可能导致训练loss和验证loss很低，但是 test loss 却很高
 
-        if self.features == 'M' or self.features == 'MS':  # 多变量
-            cols_data = df_raw.columns[1:]  # 列:['0',...,'321','OT']
-            df_data = df_raw[cols_data]  # 去掉第一列(时间列)的数据
-        elif self.features == 'S':     # 单变量
+        border1s = [0, num_train - self.seq_len, len(df_raw) - num_test - self.seq_len]
+        border2s = [num_train, num_train + num_vali, len(df_raw)]
+
+        border1 = border1s[self.set_type]
+        border2 = border2s[self.set_type]
+
+        if self.features in ['M', 'MS']:
+            cols_data = df_raw.columns[1:]
+            df_data = df_raw[cols_data]
+        elif self.features == 'S':
             df_data = df_raw[[self.target]]
 
-        if self.scale:        # 进行标准化
+        if self.scale:
             train_data = df_data[border1s[0]:border2s[0]]
-            self.scaler.fit(train_data.values)   # 参考：https://blog.csdn.net/weixin_42279212/article/details/121342576
+            self.scaler.fit(train_data.values)
             data = self.scaler.transform(df_data.values)
         else:
             data = df_data.values
 
-        df_stamp = df_raw[['date']][border1:border2]   # 当前某个模式(train/val/test)下的第一列时间数据(时间戳)
+        df_stamp = df_raw[['date']][border1:border2]
         df_stamp['date'] = pd.to_datetime(df_stamp.date)
-        if self.timeenc == 0:   # 如果 args.embed != 'timeF'，就会把时间编码为 month，day，weekday，hour 四个数
+        if self.timeenc == 0:
             df_stamp['month'] = df_stamp.date.apply(lambda row: row.month, 1)
             df_stamp['day'] = df_stamp.date.apply(lambda row: row.day, 1)
-            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)  # +1 = 星期几
+            df_stamp['weekday'] = df_stamp.date.apply(lambda row: row.weekday(), 1)
             df_stamp['hour'] = df_stamp.date.apply(lambda row: row.hour, 1)
             data_stamp = df_stamp.drop(['date'], 1).values
-        elif self.timeenc == 1:  # 默认是1，执行下列代码
-            # 这里调用了tool里面的函数time_features()，这个函数把事件戳分解为四部分：[HourOfDay, DayOfWeek,DayOfMonth, DayOfYear]
-            data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)  # 根据传入的 freq 对时间戳进行解析
-            data_stamp = data_stamp.transpose(1, 0)  # 矩阵的转置[4,18412] ==> [18412,4]
+        elif self.timeenc == 1:
+            data_stamp = time_features(pd.to_datetime(df_stamp['date'].values), freq=self.freq)
+            data_stamp = data_stamp.transpose(1, 0)
 
         self.data_x = data[border1:border2]
         self.data_y = data[border1:border2]
@@ -302,6 +287,8 @@ class Dataset_Custom(Dataset):
 
     def inverse_transform(self, data):
         return self.scaler.inverse_transform(data)
+
+
     
 
 class Dataset_Pred(Dataset):
